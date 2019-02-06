3f0585733238bafb2ed97820b976edae506bb1f8
ritoco <38111030+ritoco@users.noreply.github.com>
2019-02-06 14:53:52 +0900

    20190206_up
    File_rename

---
+++ p5.js/20190206/lining_up_rect_02.js
@@ -0,0 +1,23 @@
+
+function setup() {
+  createCanvas(2500, 2500,P2D);
+  pixelDensity(1);
+  noStroke();
+  colorMode(HSB,360,100,100,100);
+	rectMode(CENTER);
+
+  lining_up = random(150); //ここを変えた！四角の敷き詰め方がランダム。
+  noLoop(); // Run once and stop
+}
+
+function draw() {
+      background(255);
+
+  for (let x = 0; x < width; x += spacer) {
+    for (let y = 0; y < height; y += spacer) {
+      fill(random(100,130),100,random(100),random(1,20));
+      rect(x + spacer / 2, y + spacer / 2,50,350);
+      rect(x + spacer / 2, y + spacer / 2,350,50);
+    }
+  }
+}
