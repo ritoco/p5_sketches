0958620dc463b481ddc5bd1f03157b826262ba9a
ritoco <38111030+ritoco@users.noreply.github.com>
2019-03-19 15:53:54 +0900

    20190319_up

--- p5.js/20190206/lining_up_rect_master.js
+++ p5.js/20190206/lining_up_rect_master.js
@@ -4,7 +4,7 @@ function setup() {
   pixelDensity(1);
   noStroke();
   colorMode(HSB,360,100,100,100);
-	rectMode(CENTER);
+  rectMode(CENTER);
 
   spacer = random(150); //四角の敷き詰め方がランダムなのでいろんなパターンできる。
   noLoop();
@@ -15,7 +15,7 @@ function draw() {
 
   for (let x = 0; x < width; x += spacer) {
     for (let y = 0; y < height; y += spacer) {
-      fill(random(100,130),100,random(100),random(1,20));
+      fill(random(100,130),100,random(100),random(20));
       //縦横に四角を置いていく。
       rect(x + spacer / 2, y + spacer / 2,50,350);
       rect(x + spacer / 2, y + spacer / 2,350,50);
