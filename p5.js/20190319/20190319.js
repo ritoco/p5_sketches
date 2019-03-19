04c882d1231e4291d1e22f84310c58d586f4e8c2
ritoco <38111030+ritoco@users.noreply.github.com>
2019-03-19 16:18:34 +0900

    20190319
    add & change

---
+++ p5.js/20190319/20190319.js
@@ -0,0 +1,27 @@
+//ellipse + BlendMode
+var spacer;
+function setup() {
+  createCanvas(1000, 1000,P2D);
+  pixelDensity(1);
+  noStroke();
+  colorMode(HSB,360,100,100,100);
+  background(0);
+  //描画する図形の配置をランダムにする
+  spacer = random(150);
+  noLoop();
+}
+
+function draw() {
+
+  for (let x = 0; x < width; x += spacer) {
+    for (let y = 0; y < height; y += spacer) {
+
+			//HSBと図形の透明度の指定
+			fill(random(210,350), 100 ,random(100),random(10));
+
+      //縦横に図形を置いていく。
+      ellipse(x + spacer / 2, y + spacer / 2,50,350);
+      ellipse(x + spacer / 2, y + spacer / 2,350,50);
+    }
+  }
+}
