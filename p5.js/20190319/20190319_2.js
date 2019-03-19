4b26677686c4d7a6f1f88fa429bcf51eedbbc1ab
ritoco <38111030+ritoco@users.noreply.github.com>
2019-03-19 16:36:01 +0900

    20190319_add

--- p5.js/20190319/20190319_2.js
+++ p5.js/20190319/20190319_2.js
@@ -1,7 +1,7 @@
 //ellipse + BlendMode
 var spacer;
 function setup() {
-  createCanvas(2500, 2500,P2D);
+  createCanvas(1500, 1500,P2D);
 	blendMode(ADD);
   pixelDensity(1);
   noStroke();
@@ -18,11 +18,11 @@ function draw() {
     for (let y = 0; y < height; y += spacer) {
 
 			//HSBと図形の透明度の指定
-			fill(random(210,350), 100 ,random(100),random(40));
+			fill(random(80,350), 100 ,random(100),random(10));
 
       //縦横に図形を置いていく。
-      ellipse(x + spacer / 2, y + spacer / 2,50,350);
-      ellipse(x + spacer / 2, y + spacer / 2,350,50);
+      ellipse(x + spacer / 2, y + spacer / 2,150,150);
+      ellipse(x + spacer / 2, y + spacer / 2,150,150);
     }
   }
 }
