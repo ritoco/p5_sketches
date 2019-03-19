46aa64f6ca445a2a6587e081913d3210239994bb
ritoco <38111030+ritoco@users.noreply.github.com>
2019-03-19 16:00:29 +0900

    20190319_up

--- p5.js/20190206/lining_up_rect_master.js
+++ p5.js/20190206/lining_up_rect_master.js
@@ -1,6 +1,6 @@
 var spacer;
 function setup() {
-  createCanvas(2500, 2500,P2D);
+  createCanvas(1000, 1000,P2D);
   pixelDensity(1);
   noStroke();
   colorMode(HSB,360,100,100,100);
@@ -15,7 +15,10 @@ function draw() {
 
   for (let x = 0; x < width; x += spacer) {
     for (let y = 0; y < height; y += spacer) {
-      fill(random(100,130),100,random(100),random(20));
+      
+			//HSBと透明度の指定
+			fill(random(100,130), 100 ,random(100),random(20));
+			
       //縦横に四角を置いていく。
       rect(x + spacer / 2, y + spacer / 2,50,350);
       rect(x + spacer / 2, y + spacer / 2,350,50);
