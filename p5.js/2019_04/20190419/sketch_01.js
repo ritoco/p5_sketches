7b4692e27f0201aa8aecd39407f78ccc914229b1
ritoco <38111030+ritoco@users.noreply.github.com>
2019-04-20 00:16:10 +0900

    Update 20190418.js

--- p5.js/2019_04/20190419/sketch_01.js
+++ p5.js/2019_04/20190419/sketch_01.js
@@ -1,30 +1,28 @@
 //20190419_01
 
-
-// noise_false
 function setup(){
-createCanvas(1000, 1000);
-pixelDensity();
-colorMode(HSB, 360, 100, 100);
-background(225,100,50);
-ellipse_draw();
-
+  createCanvas(1000, 1000);
+  pixelDensity();
+  colorMode(HSB, 360, 100, 100);
+  background(225,100,50);
+  ellipse_draw();
 }
 
  function ellipse_draw(){
 
-for (var tate = 1; tate < height; tate += 30) {
-for (var yoko = 1; yoko < width; yoko += 30) {
+    for (var tate = 1; tate < height; tate += 30) {
+      for (var yoko = 1; yoko < width; yoko += 30) {
 
- 
+        // 線の色を 1（赤）- 80（黄緑） の間でランダムに変える
+        stroke( random(1,80),100,100 );
 
-stroke( random(1,80),100,100 );
- strokeWeight( random(1,15) );
-fill( 340,100,100 );
-ellipse(   yoko + 5 , + tate + 6 ,
-			20 , 20  );
+        // 線の太さを 1px-15px のランダムに変える
+        strokeWeight( random(1,15) );
 
+        fill( 340,100,100 );
+
+        ellipse(   yoko + 5 , + tate + 6 , 20 , 20  );
+
+      }
+    }
 }
-}
-}
-
--- p5.js/2019_04/20190419/sketch_02.js
+++ p5.js/2019_04/20190419/sketch_02.js
@@ -1,31 +1,29 @@
-
-
 //20190419_02
 
+// ノイズ範囲を決める
 var noiseRange = 50.0;
 
 function setup(){
-createCanvas(1000, 1000);
-pixelDensity();
-colorMode(HSB, 360, 100, 100);
-background(225,100,50);
-ellipse_draw();
-
+  createCanvas(1000, 1000);
+  pixelDensity();
+  colorMode(HSB, 360, 100, 100);
+  background(225,100,50);
+  ellipse_draw();
 }
 
  function ellipse_draw(){
 
-for (var tate = 1; tate < height; tate += 30) {
-for (var yoko = 1; yoko < width; yoko += 30) {
+   for (var tate = 1; tate < height; tate += 30) {
+     for (var yoko = 1; yoko < width; yoko += 30) {
 
-   var my_noise = noise(sin(noiseRange)+yoko*noiseRange);
+       // noiseを作成。なんでyokoをかけたのかはわからない。
+       var my_noise = noise( sin(noiseRange) + yoko * noiseRange );
 
-stroke( random(1,80),100,100 );
- strokeWeight( random(1,15)*my_noise );
-fill( 340,random(100),100 );
-ellipse( my_noise + yoko + 5 , my_noise + tate + 6 ,
-			20 + my_noise, 20 + my_noise );
+       stroke( random(1,80),100,100 );
+       strokeWeight( random(1,15)*my_noise );
+       fill( 340,random(100),100 );
+       ellipse( my_noise + yoko + 5 , my_noise + tate + 6 ,	20 + my_noise, 20 + my_noise );
 
-}
-}
+     }
+   }
 }
